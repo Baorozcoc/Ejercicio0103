900710343fa431fff8c0c91bae88fffcc69f408b
Estudiante <Estudiante@10.203.1.202>
2018-03-01 08:33:18 -0500

    0103

---
+++ src/automovil/Puerta.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package automovil;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Puerta {
+    private String tipo;
+    
+    public Puerta(){
+        
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+}
